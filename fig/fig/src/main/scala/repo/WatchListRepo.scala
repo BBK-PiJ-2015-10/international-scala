dadd97d3eab15ff98506ace9ea83d498e9ccb373
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2023-07-28 10:15:01 -0400

    finalizing repo

--- fig/fig/src/main/scala/repo/WatchListRepo.scala
+++ fig/fig/src/main/scala/repo/WatchListRepo.scala
@@ -1,8 +1,26 @@
 package org.fig
 package repo
 
+import model.WatchList
+
 trait WatchListRepo {
 
-  def fetchWathList() : List
+  def findAll() : List[WatchList]
+
+  def save(watchList: List[WatchList]) : List[WatchList]
 
 }
+
+object WatchListRepo extends WatchListRepo {
+
+  var storedWatchList : List[WatchList] = List()
+
+  override def findAll(): List[WatchList] = storedWatchList
+
+  override def save(watchList: List[WatchList]): List[WatchList] = {
+    storedWatchList = watchList
+    watchList
+  }
+
+}
+
