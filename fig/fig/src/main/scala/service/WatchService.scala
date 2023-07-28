26ccb514257d55fab2febcb77479eee346d94fe0
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2023-07-28 11:00:39 -0400

    adding implementation

--- fig/fig/src/main/scala/service/WatchService.scala
+++ fig/fig/src/main/scala/service/WatchService.scala
@@ -5,21 +5,38 @@ import model.Watch
 
 import org.fig.repo.WatchRepo
 
+import scala.annotation.unused
+
 trait WatchService {
 
   def fetchWatchList(): List[Watch]
 
+  def saveWatch(watch: Watch): Watch
 
 }
 
 
 case class WatchServiceImpl(watchRepo: WatchRepo) extends WatchService {
+
   override def fetchWatchList(): List[Watch] = {
     implicit val orderingByPreferenceAsc: Ordering[Watch] = Ordering.by((w: Watch) => w.preference)
     val remoteList = watchRepo.findAll()
+    //remoteList.sortWith((w1,w2) => w1.preference < w2.preference)
     remoteList.sorted
   }
 
-  
+  override def saveWatch(watch: Watch): Watch = {
+    val remoteList = fetchWatchList()
+    if (remoteList.isEmpty) {
+      val partitionedList: (List[Watch], List[Watch]) = remoteList.partition(remote => remote.preference < watch.preference)
+      val updatedRemotes = partitionedList._2.map(remoteHigher => remoteHigher.copy(preference = remoteHigher.preference + 1))
+      val updatedList = partitionedList._1 ++ List(watch) ++ updatedRemotes
+      watchRepo.save(updatedList)
+    } else {
+      watchRepo.save(List(watch))
+    }
+    watch
+  }
+
 
 }
