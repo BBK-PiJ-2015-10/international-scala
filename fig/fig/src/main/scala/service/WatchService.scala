0007898978ae8f31db73d4d667d9ee54aff3f150
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2023-07-31 09:40:15 -0400

    last changes

--- fig/fig/src/main/scala/service/WatchService.scala
+++ fig/fig/src/main/scala/service/WatchService.scala
@@ -26,13 +26,45 @@ case class WatchServiceImpl(watchRepo: WatchRepo) extends WatchService {
   }
 
   override def saveWatch(watch: Watch): Watch = {
+    val watchPreference = watch.preference
     val remoteList = fetchWatchList()
+
+
     if (remoteList.isEmpty) {
       watchRepo.save(List(watch))
     } else {
       val partitionedList: (List[Watch], List[Watch]) = remoteList.partition(remote => remote.preference < watch.preference)
       val updatedRemotes = partitionedList._2.map(remoteHigher => remoteHigher.copy(preference = remoteHigher.preference + 1))
       val updatedList = partitionedList._1 ++ List(watch) ++ updatedRemotes
+      watchRepo.save(updatedList)
+    }
+    watch
+  }
+
+  def saveWatch2(watch: Watch): Watch = {
+    var watchToSave = watch
+    val watchPreference = watch.preference
+    val remoteList = fetchWatchList()
+    if (remoteList.isEmpty) {
+      if (watchPreference != 1) {
+        watchToSave = watch.copy(preference = 1)
+      }
+      watchRepo.save(List(watchToSave))
+    } else {
+      val partitionedList: (List[Watch], List[Watch]) = remoteList.partition(remote => remote.preference < watch.preference)
+      val preferredList = partitionedList._1.sortWith((w1, w2) => w1.preference < w2.preference).reverse
+      val preferredListHead = preferredList.headOption
+      preferredListHead match {
+        case Some(preferredWatch) => if (preferredWatch.preference != watchPreference - 1) {
+          watchToSave = watch.copy(preference = preferredWatch.preference + 1)
+        }
+        case None =>
+        //if (watchPreference != 1){
+        // watchToSave = watch.copy(preference = 1)
+        //}
+      }
+      val updatedRemotes = partitionedList._2.map(remoteHigherOrEqual => remoteHigherOrEqual.copy(preference = remoteHigherOrEqual.preference + 1))
+      val updatedList = partitionedList._1 ++ List(watchToSave) ++ updatedRemotes
       watchRepo.save(updatedList)
     }
     watch
