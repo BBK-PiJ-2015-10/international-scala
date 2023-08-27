c4554204fa8b8f043d8c1853aefd60dd9e027622
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2023-08-27 12:35:58 -0400

    implementation of 401K matching calc

--- pc/pc/src/main/scala/service/ContributionCalculator.scala
+++ pc/pc/src/main/scala/service/ContributionCalculator.scala
@@ -8,34 +8,44 @@ trait ContributionCalculator {
 }
 
 case class ContributionCalculatorImpl() extends ContributionCalculator {
-  override def calculateContribution(salary: Double, desiredContributionPercentage: Double, maxContributionAmount: Double, matchingBands: Map[Double, Double]):Double = {
-    var totalAmountContribution : Double = 0.00
-    var totalLeftContributionPercentage : Double = desiredContributionPercentage
-    var totalLeftContributionAmount : Double = maxContributionAmount
-    val sortedBands = matchingBands.keySet.toList.sorted
-    for (band <- sortedBands){
-      if (totalLeftContributionAmount > 0 && totalLeftContributionPercentage > 0){
-        // simple case
-        val matchingPercentage = matchingBands.get(band)
-        if (totalLeftContributionPercentage - band < 0) {
-          // confirm amount is not exceeded
-          val matchingAmount : Double = salary * totalLeftContributionPercentage * matchingPercentage.get
-          if ((matchingAmount + totalAmountContribution) < 0) {
+  override def calculateContribution(salary: Double, desiredContributionPercentage: Double, maxContributionAmountRule: Double, matchingBands: Map[Double, Double]): Double = {
+    var maxContributionAmount: Double = 0.00
+    if (salary * desiredContributionPercentage <= maxContributionAmountRule) {
+      maxContributionAmount = salary * desiredContributionPercentage
+    } else {
+      maxContributionAmount = maxContributionAmountRule
+    }
+    calculateTotalContribution(salary, 0.0, 0.0, maxContributionAmount, 0.0, matchingBands.keySet.toList.sorted, matchingBands)
+  }
 
-          } else {
-            totalAmountContribution += matchingAmount
-            totalLeftContributionPercentage = 0
-          }
+  private def calculateTotalContribution(salary: Double, totalEmployeeContributionAmount: Double, totalCompanyContributionAmount: Double, maxContributionAmount: Double, previousBand: Double, bands: List[Double], matchingBands: Map[Double, Double]): Double = {
+    if (totalEmployeeContributionAmount >= maxContributionAmount) {
+      totalEmployeeContributionAmount + totalCompanyContributionAmount
+    } else {
+      if (bands.isEmpty) {
+        // No more matching offer by employer
+        val incrementalEmployeeContribution = maxContributionAmount - totalEmployeeContributionAmount
+        val updatedTotalEmployeeContribution = incrementalEmployeeContribution + totalEmployeeContributionAmount
+        updatedTotalEmployeeContribution + totalCompanyContributionAmount
+      } else {
+        val band = bands.head
+        val matchPercentage: Double = matchingBands(band)
+        val incrementalBand: Double = band - previousBand
+        val incrementalEmployeeContribution: Double = salary * incrementalBand
+        if (incrementalEmployeeContribution + totalEmployeeContributionAmount <= maxContributionAmount) {
+          val updatedTotalEmployeeContribution = incrementalEmployeeContribution + totalEmployeeContributionAmount
+          val updatedTotalCompanyContribution = (incrementalEmployeeContribution * matchPercentage) + totalCompanyContributionAmount
+          val remainingBands = bands.tail
+          calculateTotalContribution(salary, updatedTotalEmployeeContribution, updatedTotalCompanyContribution, maxContributionAmount, band, remainingBands, matchingBands)
         } else {
-          // confirm amount is not exceeded
-          val matchingAmount = salary * band * matchingPercentage
-
-          totalAmountContribution += matchingAmount
-          totalLeftContributionPercentage -= band
-
+          val adjustedIncrementalEmployeeContribution = maxContributionAmount - totalEmployeeContributionAmount
+          val updatedTotalEmployeeContribution = adjustedIncrementalEmployeeContribution + totalEmployeeContributionAmount
+          val updatedTotalCompanyContribution = (adjustedIncrementalEmployeeContribution * matchPercentage) + totalCompanyContributionAmount
+          val remainingBands = bands.tail
+          calculateTotalContribution(salary, updatedTotalEmployeeContribution, updatedTotalCompanyContribution, maxContributionAmount, band, remainingBands, matchingBands)
         }
       }
     }
-    totalAmountContribution
   }
+
 }
