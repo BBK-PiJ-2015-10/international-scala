7c249f0438b2974cf4d11b998923815f4a24f763
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2023-10-18 08:46:56 -0400

    solving ice cream challenge

--- pos/untitled/src/main/scala/search/SearchIceCream.scala
+++ pos/untitled/src/main/scala/search/SearchIceCream.scala
@@ -2,52 +2,39 @@ package com.pos
 package search
 
 import scala.collection.mutable.TreeMap
+import scala.collection.mutable.Set
 
 //https://medium.com/@danromans/search-code-challenge-breakdown-dadf33c0773d
 trait SearchIceCream {
 
-  def selections(array: Array[Int], budget:Int) : Set[(Int,Int)]
+  def selections(array: Array[Int], budget: Int): Set[(Int, Int)]
 
 }
 
 object SearchIceCream extends SearchIceCream {
-  override def selections(input: Array[Int],budget : Int): Set[(Int, Int)] = {
-    val costLocationsMap = TreeMap[Int,Int]()
-    Range(0,input.length).foreach(pos => {
-      costLocationsMap.put(input(pos),pos)
-    })
-    var priorAddLower = -1
-    var priorAddedHigher = -1
-    var end = input.length-1
+  override def selections(input: Array[Int], budget: Int): Set[(Int, Int)] = {
+    val result = Set[(Int, Int)]()
+    val costFlavorMap: Map[Int, IndexedSeq[Int]] = Range(0, input.length).groupMap(pos => input(pos))(pos => pos)
+    val orderedCosts = costFlavorMap.keySet.toList.sorted
+    var end = orderedCosts.size - 1
     var start = 0
-    val costs = costLocationsMap.keySet.toList
-    while (start < end){
-      val lower = costs(start)
-      val higher = costs(end)
-      val evaluator = (lower + higher) - budget
+    while (start < end) {
+      val lowerCost = orderedCosts(start)
+      val higherCost = orderedCosts(end)
+      val evaluator = (lowerCost + higherCost) - budget
       evaluator match {
         case 0 =>
-        case x if x > 0 =>
+          val lowerFlavors = costFlavorMap.get(lowerCost).get.map(_ + 1)
+          val higherFlavors = costFlavorMap.get(higherCost).get.map(_ + 1)
+          val flavors = lowerFlavors.map(lf => higherFlavors.map(hf => (hf, lf))).flatten
+          flavors.foreach(flavor => result.add(flavor))
         case _ =>
       }
-
-      //TODO:
-
+      start += 1
+      end -= 1
     }
-
-    val other = costs.
-
-
-    //for (i <-0 to 4) do {
-      //println(i)
-    //}
-    //for (i <- 0 to array.length-1) {
-      //val elem = array[i]
-    //}
-    //array.foreach(cat => costLocationsMap.)
-
-    Set()
-
+    result
   }
+
 
 }
