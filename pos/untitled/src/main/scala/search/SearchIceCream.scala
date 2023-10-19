7fc2ed71b2f7fc6fd0d0661c5fa42bc6357cc463
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2023-10-19 08:05:33 -0400

    solving issue

--- pos/untitled/src/main/scala/search/SearchIceCream.scala
+++ pos/untitled/src/main/scala/search/SearchIceCream.scala
@@ -18,20 +18,21 @@ object SearchIceCream extends SearchIceCream {
     val orderedCosts = costFlavorMap.keySet.toList.sorted
     var end = orderedCosts.size - 1
     var start = 0
-    while (start < end) {
+    var continue = true
+    while (continue) {
       val lowerCost = orderedCosts(start)
       val higherCost = orderedCosts(end)
       val evaluator = (lowerCost + higherCost) - budget
       evaluator match {
         case 0 =>
+          continue = false
           val lowerFlavors = costFlavorMap.get(lowerCost).get.map(_ + 1)
           val higherFlavors = costFlavorMap.get(higherCost).get.map(_ + 1)
           val flavors = lowerFlavors.map(lf => higherFlavors.map(hf => (hf, lf))).flatten
           flavors.foreach(flavor => result.add(flavor))
-        case _ =>
+        case _ < 0 => start +=1
+        case _ > 0 =>  end  -=1
       }
-      start += 1
-      end -= 1
     }
     result
   }
