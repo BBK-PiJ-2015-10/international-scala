d843fe2aa5d8cf749ac597005aba0991956402cd
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2023-11-30 10:11:17 -0500

    added results web server

--- hs/src/main/scala/com/hs/code/challenge/resultservice/api/ResultWebService.scala
+++ hs/src/main/scala/com/hs/code/challenge/resultservice/api/ResultWebService.scala
@@ -13,10 +13,10 @@ object ResultWebService {
       (for {
         jsonString<- req.body.asString
         _  <- ZIO.logInfo(s"woof2 $jsonString")
-        decoded= jsonString.fromJson[Country]
+        decoded= jsonString.fromJson[List[Country]]
         countries <- ZIO.fromEither(decoded)
-        _  <- ZIO.logInfo(s"woof3 ${countries.name}")
-        response <- ZIO.attempt(Response.text("Keep going"))
+        _  <- ZIO.logInfo(s"woof3 ${countries.size}")
+        response <- ZIO.attempt(Response.text(s"Submitted ${countries.size} countries"))
       } yield response).catchAll(e =>
         ZIO.succeed(Response.text(s"UnexpectedError $e").withStatus(Status.InternalServerError)))
   }
