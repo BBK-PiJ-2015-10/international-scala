c8a34e7d8431bc73309d3f94531f0def6763167b
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-01-18 09:06:11 -0500

    finalizing source a executor

--- rs/src/main/scala/com/rs/dto/Dto.scala
+++ rs/src/main/scala/com/rs/dto/Dto.scala
@@ -10,6 +10,6 @@ object Dto {
 
   case class OngoingRecord(status:String, id: String, source: String) extends Record
 
-  case class FinishedRecord(status: String, source: String) extends Record
+  case class DoneRecord(status: String, source: String) extends Record
 
 }
--- rs/src/main/scala/com/rs/service/SourceAExecutor.scala
+++ rs/src/main/scala/com/rs/service/SourceAExecutor.scala
@@ -1,21 +1,45 @@
 package com.rs.service
 
-import com.rs.dto.Dto.Record
+import com.rs.dto.Dto.{DoneRecord, OngoingRecord, Record}
+import com.rs.source.client.ApiEntities.SourceRecord
 import com.rs.source.client.SourceAClient
-import zio.{Queue, ZIO, ZLayer}
+import zio.{Queue,ZIO, ZLayer}
 
 trait SourceAExecutor {
-
-  def fetchRecords(workBuffer: Queue[Record],controlBuffer: Queue[Boolean]): ZIO[Any,Throwable,Boolean]
+  def fetchRecords(workBuffer: Queue[Record], controlBuffer: Queue[Boolean]): ZIO[Any, Throwable, Boolean]
 
 }
 
 case class SourceAExecutorImpl(sourceAClient: SourceAClient) extends SourceAExecutor {
 
-  override def fetchRecords(workBuffer: Queue[Record], controlBuffer: Queue[Boolean]): ZIO[Any, Throwable, Boolean] = {
+  override def fetchRecords(workBuffer: Queue[Record], controlBuffer: Queue[Boolean]): ZIO[Any, Throwable, Boolean] = for {
+    - <- ZIO.logInfo(s"Starting consumption of sourceA messages")
+    sourceARecord <- sourceAClient.fetchRecord()
+    result <- processRecord(workBuffer, controlBuffer, sourceARecord)
+    _ <- ZIO.logInfo("Finalizing executor")
+  } yield result
 
-
-    ZIO.attempt(true)
+  def processRecord(workBuffer: Queue[Record], controlBuffer: Queue[Boolean], record: Option[SourceRecord]): ZIO[Any, Nothing, Boolean] = {
+    if (record.isEmpty) {
+      ZIO.logInfo(s"Ignoring an empty record for sourceA") zipRight ZIO.succeed(true)
+    } else {
+      val result = record match {
+        case SourceRecord(status, None) =>
+          val record = DoneRecord(status, "sourceA")
+          for {
+            _ <- workBuffer.offer(record)
+            _ <- ZIO.logInfo(s"Offering a done record for sourceA and pausing consumption")
+            cr <- controlBuffer.take
+          } yield cr
+        case SourceRecord(status, Some(id)) =>
+          val record = OngoingRecord(status, id, "sourceA")
+          for {
+            or <- workBuffer.offer(record)
+            _ <- ZIO.logInfo(s"Offering an ongoing record for sourceA")
+          } yield or
+      }
+      result
+    }
   }
 }
 
