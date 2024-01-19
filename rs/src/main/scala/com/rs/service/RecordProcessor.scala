c577741e8f20c9ef01bb36ce295d63c2c0b962b0
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-01-19 10:03:15 -0500

    base impl of processor

--- rs/src/main/scala/com/rs/service/RecordProcessor.scala
+++ rs/src/main/scala/com/rs/service/RecordProcessor.scala
@@ -1,23 +1,51 @@
 package com.rs.service
 
-import .Record
+import com.rs.sink.client.ApiEntities.SubmissionRecord
+import com.rs.source.client.ApiEntities.SourceRecord
 import zio.{Queue, ZIO}
+
+import scala.collection.mutable.{Map, HashMap}
 
 trait RecordProcessor {
 
-  def processRecords(inputChannel: Queue[Record], outputChannel: Queue[Record]): ZIO[Any, Throwable, Boolean]
+  def processRecords(inputChannel: Queue[SourceRecord], outputChannel: Queue[SubmissionRecord]): ZIO[Any, Throwable, Boolean]
 
 }
 
 
 case class RecordProcessorImpl() extends RecordProcessor {
 
+  val records: Map[String, SourceRecord] = HashMap[String, SourceRecord]()
 
-  override def processRecords(inputChannel: Queue[Record], outputChannel: Queue[Record]): ZIO[Any, Throwable, Boolean] = {
+  var counter: Int = 0
+
+
+  private def processSourceRecored(sourceRecord: SourceRecord, outputChannel: Queue[SubmissionRecord]) = {
+    sourceRecord match {
+      case SourceRecord(_, None) =>
+        counter += 1
+        ZIO.attempt(true)
+      case SourceRecord(_, Some(id)) =>
+        records.get(id) match {
+          case None =>
+            records.put(id, sourceRecord)
+            ZIO.attempt(false)
+          case Some(r) =>
+            for {
+              offered <- outputChannel.offer(SubmissionRecord("joined", r.id.get))
+            } yield !offered
+        }
+    }
+  }
+
+  //TODO if true evaluate value of counter
+
+
+  override def processRecords(inputChannel: Queue[SourceRecord], outputChannel: Queue[SubmissionRecord]): ZIO[Any, Throwable, Boolean] = {
     for {
       _ <- ZIO.logInfo(s"Starting processing")
       inputRecord <- inputChannel.take
 
-    } yield(true)
+    } yield (true)
   }
 }
