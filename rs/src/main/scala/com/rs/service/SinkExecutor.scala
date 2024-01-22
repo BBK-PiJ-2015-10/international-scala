ee749323dafdbf2883eaf19e1485d4980125d28a
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-01-22 08:14:05 -0500

    creating sink client

---
+++ rs/src/main/scala/com/rs/service/SinkExecutor.scala
@@ -0,0 +1,26 @@
+package com.rs.service
+
+import com.rs.sink.client.ApiEntities.SubmissionRecord
+import com.rs.sink.client.SinkClient
+import zio._
+
+trait SinkExecutor {
+  def submitRecords(outputChannel: Queue[SubmissionRecord]): ZIO[Any, Throwable, Boolean]
+}
+
+case class SinkExecutorImpl(sinkClient: SinkClient) extends SinkExecutor {
+  override def submitRecords(outputChannel: Queue[SubmissionRecord]): ZIO[Any, Throwable, Boolean] = for {
+    submissionRecord <- outputChannel.take
+    _ <- ZIO.logInfo(s"Submitting response $submissionRecord")
+    response <- sinkClient.submitRecord(submissionRecord)
+    _ <- ZIO.logInfo(s"Received submission response")
+  } yield response.isDefined
+}
+
+
+object SinkExecutor {
+
+  def layer(): ZLayer[SinkClient, Throwable, SinkExecutor] =
+    ZLayer.fromFunction(SinkExecutorImpl(_))
+
+}
--- rs/src/main/scala/com/rs/sink/client/SinkClient.scala
+++ rs/src/main/scala/com/rs/sink/client/SinkClient.scala
@@ -7,28 +7,29 @@ import com.rs.parser.Parser
 import zio.json.EncoderOps
 
 trait SinkClient {
-  def submitRecord(submissionRecord: SubmissionRecord): ZIO[Client, Throwable, Option[SinkResponse]]
+  def submitRecord(submissionRecord: SubmissionRecord): ZIO[Any, Throwable, Option[SinkResponse]]
 
 }
 
-case class SinkClientImpl(sinkUrl: String) extends SinkClient {
+case class SinkClientImpl(sinkUrl: String, client: Client) extends SinkClient {
 
   val url = URL.decode(sinkUrl).toOption.get
 
-  override def submitRecord(submissionRecord: SubmissionRecord): ZIO[Client, Throwable, Option[SinkResponse]] = for {
-    client <- ZIO.service[Client]
-    jsonRequest = submissionRecord.toJson
-    body = Body.fromString(jsonRequest)
-    response <- client.url(url).request(Method.POST, "/sink/a", body)
-    jsonResponse <- response.body.asString
-  } yield Parser.jsonStringToSinkResponse(jsonResponse)
+  override def submitRecord(submissionRecord: SubmissionRecord): ZIO[Any, Throwable, Option[SinkResponse]] = {
+    val jsonRequest = submissionRecord.toJson
+    val body = Body.fromString(jsonRequest)
+    for {
+      response <- client.url(url).request(Method.POST, "/sink/a", body)
+      jsonResponse <- response.body.asString
+    } yield Parser.jsonStringToSinkResponse(jsonResponse)
+  }
 
 }
 
 object SinkClient {
 
-  def live(url: String): ZLayer[Any, Throwable, SinkClientImpl] = ZLayer.fromZIO(
-    ZIO.attempt(SinkClientImpl(url))
+  def live(url: String): ZLayer[Client, Throwable, SinkClient] = ZLayer.fromFunction(
+    SinkClientImpl(url, _)
   )
 }
 
