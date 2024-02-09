66dd8ff3b603452e0052e2ac2125df5943cdc2a1
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-02-09 09:04:15 -0500

    solving permutations

--- challenges/src/main/scala/dynamic/PermutationCalculator.scala
+++ challenges/src/main/scala/dynamic/PermutationCalculator.scala
@@ -1,12 +1,16 @@
 package dynamic
 
-import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object PermutationCalculator {
 
-  def calculatePermutations(word: String): List[String] = {
-    helper(List(), word)
+  def calculatePermutations(input: String): List[String] = {
+    if (input.isEmpty) {
+      List()
+    } else {
+      val word: List[String] = input.toCharArray.map(_.toString).toList
+      word.foldRight(List(""))((a, b) => helper(b, a))
+    }
   }
 
   def helper(ongoingPermutations: List[String], letter: String): List[String] = {
@@ -21,11 +25,10 @@ object PermutationCalculator {
         List(word)
       } else {
         val updatedPermutations: ListBuffer[String] = ListBuffer()
-        val range = Range(0, word.length+1)
+        val range = Range(0, word.length + 1)
         println(word.length)
         for (r <- range) {
           val (s1, s2) = word.splitAt(r)
-          println(s"r at $r has s1: $s1 and s2 $s2")
           val updated = s1 + character + s2
           updatedPermutations.addOne(updated)
         }
--- challenges/src/test/scala/dynamic/PermutationCalculatorSpec.scala
+++ challenges/src/test/scala/dynamic/PermutationCalculatorSpec.scala
@@ -4,41 +4,67 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class PermutationCalculatorSpec extends AnyFunSuite {
 
-  test("permutation of a single letter") {
+  test("updatePermutations of a single letter") {
     val word = ""
     val letter = "a"
     val expectedResponse = List("a")
-    val response = PermutationCalculator.updatePermutations(word,letter)
+    val response = PermutationCalculator.updatePermutations(word, letter)
     assert(response == expectedResponse)
 
   }
 
-  test("permutation of an empty letter") {
+  test("updatePermutations of an empty letter") {
     val word = "abc"
     val letter = ""
     val expectedResponse = List("abc")
-    val response = PermutationCalculator.updatePermutations(word,letter)
+    val response = PermutationCalculator.updatePermutations(word, letter)
     assert(response == expectedResponse)
 
   }
 
-  test("permutation of two letters") {
+  test("updatePermutations of two letters") {
     val word = "a"
     val letter = "b"
-    val expectedResponse = List("ab","ba")
-    val response = PermutationCalculator.updatePermutations(word,letter)
+    val expectedResponse = List("ab", "ba")
+    val response = PermutationCalculator.updatePermutations(word, letter)
     assert(response.sorted == expectedResponse.sorted)
   }
 
-  test("permutation of three letters") {
+  test("updatePermutations of three letters") {
     val word = "ab"
     val letter = "c"
-    val expectedResponse = List("cab","acb","abc")
-    val response = PermutationCalculator.updatePermutations(word,letter)
+    val expectedResponse = List("cab", "acb", "abc")
+    val response = PermutationCalculator.updatePermutations(word, letter)
     assert(response.sorted == expectedResponse.sorted)
   }
 
+  test("permutations of empty letter word") {
+    val word = ""
+    val expectedResponse = List()
+    val response = PermutationCalculator.calculatePermutations(word)
+    assert(response == expectedResponse)
+  }
 
+  test("permutations of one letter word") {
+    val word = "a"
+    val expectedResponse = List("a")
+    val response = PermutationCalculator.calculatePermutations(word)
+    assert(response.sorted == expectedResponse.sorted)
+  }
+
+  test("permutations of two letter word") {
+    val word = "ab"
+    val expectedResponse = List("ab", "ba")
+    val response = PermutationCalculator.calculatePermutations(word)
+    assert(response.sorted == expectedResponse.sorted)
+  }
+
+  test("permutations of three letter word") {
+    val word = "abc"
+    val expectedResponse = List("cab", "acb", "abc", "cba", "bca", "bac")
+    val response = PermutationCalculator.calculatePermutations(word)
+    assert(response.sorted == expectedResponse.sorted)
+  }
 
 
 }
